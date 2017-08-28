4f521c12f69982a2eccd8050ae4afe4234c014c3
sli41 <sli41@ncsu.edu>
2017-08-28 17:32:14 -0400

    initialize code

---
+++ code/ruby_intro.rb
@@ -0,0 +1,33 @@
+# Part 1
+
+def unique_total a
+  # ADD YOUR CODE HERE
+end
+
+def highest_frequency a
+  # ADD YOUR CODE HERE
+end
+
+def check_sum? a, n
+  # ADD YOUR CODE HERE
+end
+
+# Part 2
+
+def custom_concat s
+  # ADD YOUR CODE HERE
+end
+
+def initial_consonant? s
+  # ADD YOUR CODE HERE
+end
+
+def divisible_by_n? n,s
+  # ADD YOUR CODE HERE
+end
+
+# Part 3
+
+class Car
+# ADD YOUR CODE HERE
+end
