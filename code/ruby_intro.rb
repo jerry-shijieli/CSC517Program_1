ee06224647d7faf9554c0381e0dddd07d8f672c7
sli41 <sli41@ncsu.edu>
2017-08-28 18:14:57 -0400

    add instruction comments to methods

--- code/ruby_intro.rb
+++ code/ruby_intro.rb
@@ -2,28 +2,52 @@
 
 def unique_total a
   # ADD YOUR CODE HERE
+  # takes an array of integers as an argument and then returns the
+  # sum of unique elements in the array. The sum of an empty array is zero.
+
 end
 
 def highest_frequency a
   # ADD YOUR CODE HERE
+  # # which takes an array of integers as an argument and
+  # returns the element with highest frequency. If there is a tie, return the smallest
+  # element. For empty array, return -1.
+
 end
 
 def check_sum? a, n
   # ADD YOUR CODE HERE
+  # that takes an array of integers and an additional integer, n, as arguments
+  # and returns true if any two elements in the array of integers sum
+  # to n. checkSum?([], n) should return false for any value of n, by definition.
+
 end
 
 # Part 2
 
 def custom_concat s
   # ADD YOUR CODE HERE
+  # takes a string as an argument and returns the string
+  # "Welcome, " concatenated at the beginning of the input string.
+
 end
 
 def initial_consonant? s
   # ADD YOUR CODE HERE
+  # takes a string as an argument and returns a boolean
+  # value. It is true if it starts with a consonant and false otherwise. (Any character
+  # other than a vowel is a consonant) This method should work for both uppercase
+  # and lowercase strings. Return false if string is empty or null.
+
 end
 
 def divisible_by_n? n,s
   # ADD YOUR CODE HERE
+  # takes a string as an argument and returns a boolean
+  # value. It is true if the string represents a binary number that is exactly divisible by
+  # ‘n’ and false otherwise. It should return false even if the argument is not a valid
+  # binary number.
+
 end
 
 # Part 3
